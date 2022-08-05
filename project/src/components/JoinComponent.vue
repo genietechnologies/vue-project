117f8bb5780dc2805910d22b2af62de6d793c20b
genie <jjlee@genietechnologies.co.kr>
2022-08-05 10:51:31 +0900

    jjlee 08/05 10:51

--- project/src/components/JoinComponent.vue
+++ project/src/components/JoinComponent.vue
@@ -4,8 +4,11 @@
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
-            <v-alert v-if="validations === false" type="error"
+            <v-alert v-if="validations === 1" type="error"
               >필수 입력사항 입니다.</v-alert
+            >
+            <v-alert v-if="validations === 2" type="error"
+              >아이디가 중복 되었습니다.</v-alert
             >
             <h1>회원가입</h1>
             <v-text-field label="아이디" v-model="id"></v-text-field>
@@ -34,6 +37,7 @@ export default {
       name: null,
       email: null,
       validations: null,
+      isDuplicated: false,
     };
   },
   methods: {
@@ -45,8 +49,11 @@ export default {
         name: this.name,
         email: this.email,
       };
+      this.validations = null;
+      this.isDuplicated = false;
+      console.log(this.validations, this.isDuplicated);
       this.validationCheck(userObj);
-      if (this.validations === true) {
+      if (this.validations === 3) {
         this.addUserAction(userObj);
         this.clearForm();
         this.$router.push({ name: "home" });
@@ -56,19 +63,33 @@ export default {
       (this.id = null),
         (this.password = null),
         (this.name = null),
-        (this.email = null);
+        (this.email = null),
+        (this.validations = null),
+        (this.isDuplicated = false);
     },
     validationCheck(data) {
+      this.duplicationCheck(data);
       if (
         data.id === null ||
+        data.id === "" ||
         data.password === null ||
+        data.password === "" ||
         data.name === null ||
-        data.email === null
+        data.name === "" ||
+        data.email === null ||
+        data.email === ""
       ) {
-        return (this.validations = false);
+        return (this.validations = 1);
+      } else if (this.isDuplicated === true) {
+        return (this.validations = 2);
       } else {
-        return (this.validations = true);
+        return (this.validations = 3);
       }
+    },
+    duplicationCheck(data) {
+      this.$store.state.allUsers.forEach((user) => {
+        if (user.id === data.id) this.isDuplicated = true;
+      });
     },
   },
 };
